66ba933e5b634d6bd4a12b787439b6c993041be9
Ari Archer <ari.web.xyz@gmail.com>
2024-02-20 05:08:41 +0200

    update @ Tue Feb 20 05:08:41 EET 2024
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/blog.py
+++ scripts/blog.py
@@ -1313,6 +1313,7 @@ def rss(config: dict[str, typing.Any]) -> int:
             post["created"]
         ).strftime(ftime)
         etree.SubElement(item, "guid").text = link
+        etree.SubElement(item, "author").text = f"{config['email']} ({config['author']})"
 
     etree.ElementTree(root).write(
         config["rss-file"], encoding="UTF-8", xml_declaration=True
